fa6c0d561f4acfbcba76a110467caf900a11fb9c
Phani Sai Kamal Lingam <lingamsai3@gmail.com>
2020-04-06 14:07:27 -0700

    update data and output

--- app.R
+++ app.R
@@ -65,7 +65,133 @@ ui <- navbarPage(title = "COVID-19 Monitor",
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    output$confirmedMap <- renderLeaflet(
+        leaflet(df, options = leafletOptions(minZoom = 1, maxZoom = 5)) %>%  
+            addTiles() %>% 
+            addCircles(lng = ~Long, lat = ~Lat, weight = 1, 
+                       radius = ~Confirmed*5, label = ~as.character(gsub("NA ,", "", 
+                                                                         paste(State, ",", 
+                                                                               Country, "-", 
+                                                                               "Confirmed: ", Confirmed))), 
+                       labelOptions = labelOptions(noHide = FALSE), 
+                       fillOpacity = 0.3, color = "red") %>% 
+            setView(lng = 0, lat = 0, zoom = 1.5) %>% 
+            setMaxBounds(-180, 90, 180, -90) 
+    )
     
+    output$deathsMap <- renderLeaflet(
+        leaflet(df, options = leafletOptions(minZoom = 1, maxZoom = 5)) %>% 
+            addTiles() %>% 
+            addCircles(lng = ~Long, lat = ~Lat, weight = 1, 
+                       radius = ~Deaths*5, label = ~as.character(gsub("NA ,", "", 
+                                                                      paste(State, ",", 
+                                                                            Country, "-", 
+                                                                            "Deaths: ", Deaths))), 
+                       labelOptions = labelOptions(noHide = FALSE), 
+                       fillOpacity = 0.3, color = "purple") %>% 
+            setView(lng = 0, lat = 0, zoom = 1.5) %>% 
+            setMaxBounds(-180, 90, 180, -90) 
+    )
+    
+    output$recoveredMap <- renderLeaflet(
+        leaflet(df, options = leafletOptions(minZoom = 1, maxZoom = 5)) %>% 
+            addTiles() %>% 
+            addCircles(lng = ~Long, lat = ~Lat, weight = 1, 
+                       radius = ~Recovered*5, label = ~as.character(gsub("NA ,", "", 
+                                                                         paste(State, ",", 
+                                                                               Country, "-", 
+                                                                               "Recovered: ", Recovered))), 
+                       labelOptions = labelOptions(noHide = FALSE), 
+                       fillOpacity = 0.3, color = "green") %>% 
+            setView(lng = 0, lat = 0, zoom = 1.5) %>% 
+            setMaxBounds(-180, 90, 180, -90) 
+    )
+    
+    output$activeMap <- renderLeaflet(
+        leaflet(df, options = leafletOptions(minZoom = 1, maxZoom = 5)) %>% 
+            addTiles() %>% 
+            addCircles(lng = ~Long, lat = ~Lat, weight = 1, 
+                       radius = ~Active*5, label = ~as.character(gsub("NA ,", "", 
+                                                                      paste(State, ",", 
+                                                                            Country, "-", 
+                                                                            "Active: ", Active))), 
+                       labelOptions = labelOptions(noHide = FALSE), 
+                       fillOpacity = 0.3, color = "orange") %>% 
+            setView(lng = 0, lat = 0, zoom = 1.5) %>% 
+            setMaxBounds(-180, 90, 180, -90) 
+    )
+    
+    output$confirmedTable <- DT::renderDataTable(
+        datatable(
+            df %>% 
+                group_by(Country) %>% 
+                summarise(Total = sum(Confirmed)) %>% 
+                arrange(-Total) %>% 
+                head(10), 
+            rownames = FALSE, 
+            options = list(searching = FALSE, 
+                           paging = FALSE, 
+                           info = FALSE)
+        ) %>% 
+            formatStyle(
+                c('Country', 'Total'), 
+                color = 'black', backgroundColor = NULL, fontWeight = 'bold'
+            )
+    )
+    
+    output$deathsTable <- DT::renderDataTable(
+        datatable(
+            df %>% 
+                group_by(Country) %>% 
+                summarise(Total = sum(Deaths)) %>% 
+                arrange(-Total) %>% 
+                head(10), 
+            rownames = FALSE, 
+            options = list(searching = FALSE, 
+                           paging = FALSE, 
+                           info = FALSE)
+        ) %>% 
+            formatStyle(
+                c('Country', 'Total'), 
+                color = 'black', backgroundColor = NULL, fontWeight = 'bold'
+            )
+    )
+    
+    output$recoveredTable <- DT::renderDataTable(
+        datatable(
+            df %>% 
+                group_by(Country) %>% 
+                summarise(Total = sum(Recovered)) %>% 
+                arrange(-Total) %>% 
+                head(10), 
+            rownames = FALSE, 
+            options = list(searching = FALSE, 
+                           paging = FALSE, 
+                           info = FALSE)
+        ) %>% 
+            formatStyle(
+                c('Country', 'Total'), 
+                color = 'black', backgroundColor = NULL, fontWeight = 'bold'
+            )
+    )
+    
+    output$activeTable <- DT::renderDataTable(
+        datatable(
+            df %>% 
+                group_by(Country) %>% 
+                summarise(Total = sum(Active)) %>% 
+                arrange(-Total) %>% 
+                head(10), 
+            rownames = FALSE, 
+            options = list(searching = FALSE, 
+                           paging = FALSE, 
+                           info = FALSE)
+        ) %>% 
+            formatStyle(
+                c('Country', 'Total'), 
+                color = 'black', backgroundColor = NULL, fontWeight = 'bold'
+            )
+    )
 }
 
 # Run the application 
